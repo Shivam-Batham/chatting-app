08647fc4432686c55a612a867203c47e35c949c2
shivam-batham <shivambatha2610@gmail.com>
2025-04-18 16:20:42 +0530

    fix

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 import { ExpresBaseUrl } from "../constants";
-import Cookies from 'js-cookie'; 
 
 export const axiosInstance = axios.create({
   baseURL: ExpresBaseUrl,
@@ -9,16 +8,3 @@ export const axiosInstance = axios.create({
     'Content-Type': 'application/json'
   }
 });
-
-axiosInstance.interceptors.request.use(
-  (config) => {
-    const token = Cookies.get('jwt'); 
-    if (token) {
-      config.headers['Authorization'] = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
